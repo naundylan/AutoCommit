34cace53fcd69bb38d4f3754ca82a34cff0edc51
quyen <naundylan@gmail.com>
2024-11-05 21:50:38 +0700

    update

---
+++ auto_commit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Đường dẫn đến thư mục Git repo của bạn
+REPO_PATH="D:\Practice\AutoCommit"
+
+# Thông báo commit
+COMMIT_MESSAGE="Daily commit to keep the streak going"
+
+# Di chuyển vào thư mục repo
+cd $REPO_PATH
+
+# Tạo một thay đổi giả (chẳng hạn như cập nhật timestamp)
+date > dummy_file.txt
+
+# Thêm thay đổi vào staging
+git add dummy_file.txt
+
+# Commit thay đổi
+git commit -m "$COMMIT_MESSAGE"
+
+# Push lên GitHub
+git push origin main
